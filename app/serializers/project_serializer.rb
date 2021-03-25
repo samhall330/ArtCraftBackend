523e4076b806042ea21a25fc1f0a09c7ee2e1c1e
Sam Hall <mann.samantha11@gmail.com>
2021-03-24 19:53:43 -0700

    serializer changes

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,4 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :user_id, :location, :duration, :project_type
+  attributes :id, :title, :user_id, :location, :duration, :project_type, :start_date
   has_many :collaborators
 end
