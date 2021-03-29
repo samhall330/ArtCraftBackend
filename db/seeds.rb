54a06f46e08a59228ca18e1942f87d58e0aba97f
Sam Hall <mann.samantha11@gmail.com>
2021-03-29 12:07:00 -0700

    seeds for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ video_editing = Specialty.create(user_id: 1, name: "video editing", pro_level: "
 audio = Specialty.create(user_id: 2, name: "audio", pro_level: "Semi-Professional")
 audio_recording = Specialty.create(user_id: 4, name: "audio recording", pro_level: "Professional")
 acting = Specialty.create(user_id: 2, name: "acting", pro_level: "Professional")
+painting = Specialty.create(user_id: 3, name: "painting", pro_level: "Semi-Professional")
 ### Equipment ###
 
 camera_tripod = Equipment.create(user_id: 1, name: "camera tripod", equip_link: "https://www.amazon.com/AmazonBasics-60-Inch-Lightweight-Tripod-Bag/dp/B005KP473Q/ref=asc_df_B005KP473Q/?tag=hyprod-20&linkCode=df0&hvadid=167139312229&hvpos=&hvnetw=g&hvrand=5992561659332765815&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9026544&hvtargid=pla-124905856875&psc=1")
@@ -25,7 +26,7 @@ adobe_audition = Software.create(user_id: 4, name: "Adobe Audition", soft_link: 
 
 ### Projects ###
 
-short_film = Project.create(user_id: 1, title: "Short Film", project_type: "short film", location: "New York City", start_date: "May 1, 2021", duration: "3 weeks")
-comedy = Project.create(user_id: 1, title: "Comedy Show", project_type: "comedy show", location: "New York City", start_date: "March 30, 2021", duration: "1 night")
-art_show = Project.create(user_id: 1, title: "Mod Art Show", project_type: "art show", location: "New York City", start_date: "July 1st, 2021", duration: "1 week")
+short_film = Project.create(user_id: 1, title: "Le Film Court", project_type: "short film", location: "New York City", start_date: "May 1, 2021", duration: "3 weeks")
+comedy = Project.create(user_id: 1, title: "LMAO Comedy Show", project_type: "comedy show", location: "New York City", start_date: "March 30, 2021", duration: "1 night")
+art_show = Project.create(user_id: 1, title: "Modern Art Gallery", project_type: "art show", location: "New York City", start_date: "July 1st, 2021", duration: "1 week")
 
